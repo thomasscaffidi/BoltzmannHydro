94a6e4ac9692b5c74a748a168190a7739c91103a
Thomas Scaffidi <thomas.scaffidi@gmail.com>
2018-12-08 16:53:05 -0800

    test

---
+++ StartJobsOnCluster.pl
@@ -0,0 +1,19 @@
+print "Hello \n";
+
+$cluster="lr2";
+my $n=1;
+
+my $DirName = "./Tests/";
+my $BatchFileName = "BaseForScript_cluster_${cluster}_n_${n}.sh";
+
+system("mkdir ".$DirName);
+chdir($DirName);
+system("cp ../*.m .");
+system("cp ../" . $BatchFileName ." .");
+
+$filename = "BatchScript_${DirName}.sh";
+system("cp " . $BatchFileName ." ".  $filename);
+open(my $fh, '>>', $filename) or die "Could not open file '$filename' $!";
+print $fh "\n module load matlab ";
+print $fh "\n /global/software/sl-7.x86_64/modules/tools/matlab/r2017b/bin/matlab -nodesktop -nosplash -r 'GenerateGoodData_SAS'";
+system("sbatch ".$filename);
